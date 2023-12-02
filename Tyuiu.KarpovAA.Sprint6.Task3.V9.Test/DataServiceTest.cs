d687b1af973c880bee5bc86aa2a8c39c2a586624
Андрей Карпов <karpov.andrei.000@mail.ru>
2023-12-02 18:43:31 +0500

    Task3

--- Tyuiu.KarpovAA.Sprint6.Task3.V9.Test/DataServiceTest.cs
+++ Tyuiu.KarpovAA.Sprint6.Task3.V9.Test/DataServiceTest.cs
@@ -10,6 +10,22 @@ namespace Tyuiu.KarpovAA.Sprint6.Task3.V9.Test
         [TestMethod]
         public void TestMethod1()
         {
+            DataService ds = new DataService();
+
+            int[,] matrix = { { -14,  25,  26,  18,  17 },
+                              { 28,  10,   6,  -2,   4 },
+                              { 30,  25,  -3,  11, -10 },
+                              { 11,  32,  -5, -20,  25 },
+                              { 2, -18,  11,   8, -20} };
+
+            int[,] res = ds.Calculate(matrix);
+            int[,] wait = { { -14, 25, 26, 18, -20 },
+                            { 28, 10,  6, -2, -10 },
+                            { 30, 25, -3, 11, 4},
+                            { 11, 32, -5, -20, 17 },
+                            { 2, -18, 11, 8, 25 } };
+
+            CollectionAssert.AreEqual(wait, res);
         }
     }
 }
